f84f6703c24e54d2dc002ec83ca1c77ba14feae8
goel-disha <goeldisha732@gmail.com>
2025-03-20 16:31:56 +0000

    java

--- basics.1/conversion.js
+++ basics.1/conversion.js
@@ -1,29 +1,55 @@
-let score ="33bc"
+// let score ="33bc"
 
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
-let valueinnumber = Number(score)
+// let valueinnumber = Number(score)
 
-console.log(typeof valueinnumber);
-console.log(valueinnumber);
+// console.log(typeof valueinnumber);
+// console.log(valueinnumber);
 
-// "33"=> 33
-// "33abc"=> NaN
-// true => 1; false=0;
+// // "33"=> 33
+// // "33abc"=> NaN
+// // true => 1; false=0;
 
-let isloogedin = ""
+// let isloogedin = ""
 
-let booleanloggedin = Boolean(isloogedin)
-console.log(booleanloggedin);
+// let booleanloggedin = Boolean(isloogedin)
+// console.log(booleanloggedin);
 
-// empty => false
-// "disha"=> true
+// // empty => false
+// // "disha"=> true
 
-let somenumber =33
+// let somenumber =33
 
-let stringnumber = String(somenumber)
+// let stringnumber = String(somenumber)
 
-console.log(stringnumber);
-console.log(typeof stringnumber);
+// console.log(stringnumber);
+// console.log(typeof stringnumber);
+
+// operations //
+
+// let value =3
+// let negvalue = -value
+// console.log(negvalue);
+
+// let str1 ="disha"
+// let str2 =" hello"
+
+// let str3 = str1+str2
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+let num1,num2,num3
+num1=num2=num3=2+2
+
+let gamecounter=100
+gamecounter++;
+console.log(gamecounter);
+
 
