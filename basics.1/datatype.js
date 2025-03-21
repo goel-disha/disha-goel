e21bb1af59d77f70d78ad9114a759347bf11acfc
goel-disha <goeldisha732@gmail.com>
2025-03-21 20:50:07 +0000

    java

--- basics.1/datatype.js
+++ basics.1/datatype.js
@@ -24,3 +24,59 @@ let state;
 console.log(typeof "disha");
 
 console.log(typeof null); // obejct
+
+// primitive
+
+// 7 types: string,number,boolean,null,undefined,symbol,BigInt
+
+const score=100
+const scorevalue=100.3
+
+const isloggedin =false
+const outsidetemp =null
+let useremail;
+
+const id =Symbol('123')
+const anotherid = Symbol('123')
+
+const bignumber = 25475246532654n
+
+
+// reference(non primitive)
+// array,objects,funtions 
+
+const heros =["shaktiman","naagraj","doga"]
+let myobj ={
+    name: "disha",
+    age: 22,
+}
+
+const myfunction = function(){
+    console.assertlog("hello world");
+}
+
+console.log(typeof bignumber);
+
+// ******************************
+// stack(primitive), heap(non-primitive)
+
+let myname = "dishagoel"
+
+let anothername = myname
+
+anothername="priyanshi"
+
+console.log(anothername);
+console.log(myname);
+
+let user ={
+    email : "user@google.com",
+    upi: "user@ybl"
+}
+
+let usertwo =user
+
+
+
+console.log(user.email);
+console.log(usertwo.email);
